21f1eaf88644ad3ab248d4490e7d8afaeb1d6d65
hitesh3634804 <hitesh@lifesparktech.com>
2024-06-01 15:09:08 +0530

    commented unused imports

--- lib/src/views/user/revisedaccountpage.dart
+++ lib/src/views/user/revisedaccountpage.dart
@@ -16,8 +16,8 @@ import 'package:walk/src/models/user_model.dart';
 import 'package:walk/src/utils/custom_navigation.dart';
 import 'package:walk/src/views/auth/phone_auth.dart';
 import 'package:walk/src/views/calendar/calendar_view.dart';
-import 'package:walk/src/views/physiotherapist/landing_page.dart';
-import 'package:walk/src/views/revisedhome/newhomepage.dart';
+// import 'package:walk/src/views/physiotherapist/landing_page.dart';
+// import 'package:walk/src/views/revisedhome/newhomepage.dart';
 
 String country = "India";
 Map<DateTime, List<int>> _kEventSource = {};
