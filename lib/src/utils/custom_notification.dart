01208bbf655013816701a41809f80b6fd6d2372c
hitesh3634804 <hitesh@lifesparktech.com>
2024-06-01 12:57:09 +0530

    deleted dead code

--- lib/src/utils/custom_notification.dart
+++ lib/src/utils/custom_notification.dart
@@ -1,7 +1,3 @@
-import 'dart:developer';
-import 'dart:io';
-import 'dart:math';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
@@ -197,9 +193,7 @@ class NotificationService {
           AwesomeNotifications().setGlobalBadgeCounter(0);
         },
       );
-      // AwesomeNotifications().cancel(receivedAction.id ?? 0);
     }
-    // log("coming here");
   }
 
   @pragma("vm:entry-point")
@@ -213,132 +207,3 @@ class NotificationService {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-// static Future<void> sendScheduledTestNotification(
-//       {String? title, String? body}) async {
-//     bool isPermissionGranted =
-//         await AwesomeNotifications().isNotificationAllowed();
-//     int intervalMinutes = 60 ~/ 3;
-//     if (isPermissionGranted) {
-//       for (int i = 0; i < 3; i++) {
-//         await AwesomeNotifications().createNotification(
-//           content: NotificationContent(
-//               id: DateTime.now().millisecondsSinceEpoch.remainder(100000),
-//               channelKey: 'scheduled_channel',
-//               title: title ?? "Reminder!!! Do Therapy.",
-//               body: body ?? "Don't Missout the therapy for better you.",
-//               color: Colors.white,
-//               payload: {'page': 'ConnectionScreen'}),
-//           actionButtons: [
-//             NotificationActionButton(key: 'DO_NOW', label: 'Do Now'),
-//             NotificationActionButton(key: 'MARK_DONE', label: 'Mark as Done')
-//           ],
-//           schedule: NotificationCalendar(
-//             weekday: DateTime.now().weekday,
-//             hour: DateTime.now().hour > 13 ? 17 : 11,
-//             minute: 0 + intervalMinutes * i,
-//             second: 0,
-//             millisecond: 0,
-//             repeats: true,
-//           ),
-//         );
-//       }
-//     }
-//   }
-
-// static Future<void> sendScheduledTestNotification(
-//       {String? title, String? body}) async {
-//     bool isPermissionGranted =
-//         await AwesomeNotifications().isNotificationAllowed();
-//     int intervalMinutes = 20; // Set the desired interval in minutes
-//     int totalNotifications = 3; // Number of notifications to be sent
-
-//     if (isPermissionGranted) {
-//       // Calculate the start time for notifications
-//       DateTime now = DateTime.now();
-//       DateTime startTime = DateTime(now.year, now.month, now.day, 11, 0, 0);
-//       if (now.hour > 13) {
-//         startTime = DateTime(now.year, now.month, now.day, 17, 0, 0);
-//       }
-
-//       for (int i = 0; i < totalNotifications; i++) {
-//         DateTime notificationTime =
-//             startTime.add(Duration(minutes: i * intervalMinutes));
-//         int notificationId =
-//             notificationTime.millisecondsSinceEpoch.remainder(100000);
-
-//         await AwesomeNotifications().createNotification(
-//           content: NotificationContent(
-//               id: notificationId,
-//               channelKey: 'scheduled_channel',
-//               title: title ?? "Reminder!!! Do Therapy.",
-//               body: body ?? "Don't Missout the therapy for better you.",
-//               color: Colors.white,
-//               payload: {'page': 'ConnectionScreen'}),
-//           actionButtons: [
-//             NotificationActionButton(key: 'DO_NOW', label: 'Do Now'),
-//             NotificationActionButton(key: 'MARK_DONE', label: 'Mark as Done')
-//           ],
-//           schedule: NotificationCalendar(
-//             weekday: notificationTime.weekday,
-//             hour: notificationTime.hour,
-//             minute: notificationTime.minute,
-//             second: notificationTime.second,
-//             millisecond: notificationTime.microsecond ~/ 1000,
-//             repeats: true,
-//           ),
-//         );
-//       }
-//     }
-//   }
-
-// static Future<void> sendScheduledTestNotification(
-  //     {String? title, String? body}) async {
-  //   bool isPermissionGranted =
-  //       await AwesomeNotifications().isNotificationAllowed();
-  //   int intervalMinutes = 1;
-  //   List<String> dailyMessages = [
-  //     'Bugün harika bir gün!',
-  //     'Yeni bir şeyler öğrenmeye hazır mısınız?',
-  //     'Hayalinizdeki projeye başlama zamanı!',
-  //     'Güzel anları değerlendirin!',
-  //     'Bugün bir gülümsemeyle başlayın!',
-  //   ];
-
-  //   // Rastgele bir mesaj seç
-  //   Random random = Random();
-  //   var selectedMessage = dailyMessages[random.nextInt(dailyMessages.length)];
-  //   if (isPermissionGranted) {
-  //     for (int i = 0; i < 3; i++) {
-  //       await AwesomeNotifications().createNotification(
-  //         content: NotificationContent(
-  //             id: DateTime.now().millisecondsSinceEpoch.remainder(100000),
-  //             channelKey: 'scheduled_channel',
-  //             title: title ?? selectedMessage,
-  //             body: body ?? "Don't Missout the therapy for better you.",
-  //             color: Colors.white,
-  //             payload: {'page': 'ConnectionScreen'}),
-  //         actionButtons: [
-  //           NotificationActionButton(key: 'DO_NOW', label: 'Do Now'),
-  //           NotificationActionButton(key: 'MARK_DONE', label: 'Mark as Done')
-  //         ],
-  //         schedule: NotificationCalendar(
-  //           weekday: DateTime.now().weekday,
-  //           hour: DateTime.now().hour > 13 ? 16 : 11,
-  //           minute: 32 + intervalMinutes * i,
-  //           second: 0,
-  //           millisecond: 0,
-  //           repeats: true,
-  //         ),
-  //       );
-  //     }
-  //   }
-  // }
