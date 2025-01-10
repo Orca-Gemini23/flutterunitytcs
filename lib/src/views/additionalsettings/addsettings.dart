1736426781910fd9ed5c42dcce25064518261d9d
Lifespark Technologies <hitesh@lifesparktech.com>
2025-01-10 14:31:09 +0530

    work in progress

--- lib/src/views/additionalsettings/addsettings.dart
+++ lib/src/views/additionalsettings/addsettings.dart
@@ -45,107 +45,94 @@ class _AdditionalSettingsState extends State<AdditionalSettings> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: false,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        iconTheme: const IconThemeData(
-          color: AppColor.blackColor,
+    return Column(
+      children: [
+        const SizedBox(
+          height: 10,
         ),
-        title: const Text(
-          "Additional Settings",
-          style: TextStyle(
-            color: AppColor.blackColor,
-            fontSize: 16,
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFFE8E8E8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            children: [
+              const Text(
+                "Current Mode:",
+                style: TextStyle(
+                  color: AppColor.blackColor,
+                  fontSize: 14.36,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Consumer<DeviceController>(
+                  builder: (context, deviceController, widget) {
+                return DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    iconSize: 0,
+                    value: modesDictionary[_selectedMode],
+                    items: modesDictionary.keys.map((String modeName) {
+                      return DropdownMenuItem<String>(
+                        value: modesDictionary[modeName],
+                        child: Text(
+                          modeName,
+                          style: const TextStyle(
+                            color: AppColor.blackColor,
+                            fontSize: 14.36,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) async {
+                      log(newValue!);
+                      setState(() {
+                        _selectedMode = modesDictionary.keys.firstWhere(
+                            (element) => modesDictionary[element] == newValue);
+                        if (_selectedMode == "Advanced") {
+                          AdvancedMode.modevisiable = true;
+                        } else {
+                          AdvancedMode.modevisiable = false;
+                        }
+                      });
+                      Analytics.addClicks(_selectedMode, DateTime.timestamp());
+                      ////Send Change Mode Command to device
+                      log("$MODE $newValue;");
+
+                      _storeMode();
+                      await deviceController.sendToDevice(
+                          "$MODE $newValue;", WRITECHARACTERISTICS);
+
+                      if (_selectedMode == "Advanced") {
+                        await deviceController.sendToDevice(
+                            "alx_m 1;", WRITECHARACTERISTICS);
+                        await deviceController.sendToDevice(
+                            "arx_m 1;", WRITECHARACTERISTICS);
+                        await deviceController.sendToDevice(
+                            "arx_min: -0.5;", WRITECHARACTERISTICS);
+                        await deviceController.sendToDevice(
+                            "arx_max: 0.5;", WRITECHARACTERISTICS);
+                        await deviceController.sendToDevice(
+                            "alx_min: -0.5;", WRITECHARACTERISTICS);
+                        await deviceController.sendToDevice(
+                            "alx_max: 0.5;", WRITECHARACTERISTICS);
+                      }
+                    },
+                  ),
+                );
+              }),
+              const Spacer(),
+              const Icon(
+                Icons.settings_outlined,
+                size: 20,
+              ),
+            ],
           ),
         ),
-      ),
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        padding: const EdgeInsets.only(
-          top: 20,
-          bottom: 15,
-          left: 15,
-          right: 15,
+        const SizedBox(
+          height: 10,
         ),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: AppColor.lightgreen,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  const Text(
-                    "Mode",
-                    style: TextStyle(
-                      color: AppColor.blackColor,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const Spacer(),
-                  Consumer<DeviceController>(
-                      builder: (context, deviceController, widget) {
-                    return DropdownButton<String>(
-                      value: modesDictionary[_selectedMode],
-                      items: modesDictionary.keys.map((String modeName) {
-                        return DropdownMenuItem<String>(
-                          value: modesDictionary[modeName],
-                          child: Text(modeName),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) async {
-                        log(newValue!);
-                        // deviceController.setmodeValue(newValue as int);
-                        setState(() {
-                          _selectedMode = modesDictionary.keys.firstWhere(
-                              (element) =>
-                                  modesDictionary[element] == newValue);
-                          if (_selectedMode == "Advanced") {
-                            AdvancedMode.modevisiable = true;
-                          } else {
-                            AdvancedMode.modevisiable = false;
-                          }
-                        });
-                        Analytics.addClicks(
-                            _selectedMode, DateTime.timestamp());
-                        ////Send Change Mode Command to device
-                        log("$MODE $newValue;");
-
-                        _storeMode();
-                        await deviceController.sendToDevice(
-                            "$MODE $newValue;", WRITECHARACTERISTICS);
-
-                        if (_selectedMode == "Advanced") {
-                          await deviceController.sendToDevice(
-                              "alx_m 1;", WRITECHARACTERISTICS);
-                          await deviceController.sendToDevice(
-                              "arx_m 1;", WRITECHARACTERISTICS);
-                          await deviceController.sendToDevice(
-                              "arx_min: -0.5;", WRITECHARACTERISTICS);
-                          await deviceController.sendToDevice(
-                              "arx_max: 0.5;", WRITECHARACTERISTICS);
-                          await deviceController.sendToDevice(
-                              "alx_min: -0.5;", WRITECHARACTERISTICS);
-                          await deviceController.sendToDevice(
-                              "alx_max: 0.5;", WRITECHARACTERISTICS);
-                        }
-                      },
-                    );
-                  }),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
-      ),
+      ],
     );
   }
 }
