eca6052ecf65ca746fa6e80b3ce0d8080058a02b
Orca-Gemini23 <cs.yash95@gmail.com>
2025-01-21 12:03:38 +0530

    Added files

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -9,6 +9,37 @@ public class BuildScript
     {
         // Start the Unity Android library build process
         Build.DoBuildAndroidLibraryRelease();
-        Process.Start("flutter", "build appbundle");
+
+        // Run Flutter build appbundle
+        Process process = new Process();
+        process.StartInfo.FileName = "cmd.exe"; // Use the command line
+        process.StartInfo.Arguments = "/c flutter build appbundle"; // Command to run
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+
+        process.OutputDataReceived += (sender, args) => UnityEngine.Debug.Log(args.Data); // Explicitly UnityEngine.Debug
+        process.ErrorDataReceived += (sender, args) => UnityEngine.Debug.LogError(args.Data); // Explicitly UnityEngine.Debug
+
+        try
+        {
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                UnityEngine.Debug.LogError($"Flutter build appbundle failed with exit code {process.ExitCode}");
+            }
+        }
+        catch (System.Exception ex)
+        {
+            UnityEngine.Debug.LogError($"Error running Flutter build appbundle: {ex.Message}");
+        }
+        finally
+        {
+            process.Close();
+        }
     }
-}
+}g
